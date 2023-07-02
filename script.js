1d9d005352e3a45df02fb4b719d1609fbb398f3c
Suraj JAwrani <surajjawrani2022@gmail.com>
2023-07-02 16:49:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+let string = "";
+let buttons = document.querySelectorAll(".button");
+console.log(buttons);
+
+
+Array.from(buttons).forEach( (button) => {
+    button.addEventListener("click", (e) => {
+       if(e.target.innerHTML == "=") {
+        string = eval(string);
+        document.querySelector('input').value = string;
+       }
+
+       else if(e.target.innerHTML == "AC") {
+           string = "";
+           document.querySelector('input').value = string;
+       }
+
+       else if(e.target.innerHTML == "DEL"){
+           string = string.slice(0 ,-1);
+           document.querySelector('input').value = string;
+        }
+
+
+       else {
+        string = string + e.target.innerHTML;
+        document.querySelector('input').value = string;
+       }
+    
+
+
+    })
+})
